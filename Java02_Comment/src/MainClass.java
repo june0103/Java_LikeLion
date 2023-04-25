65fa2a1373b67a2d7f19b120c42009e12bcb4d72
june0103 <rhwns0103@naver.com>
2023-04-25 22:42:31 +0900

    Comment

---
+++ Java02_Comment/src/MainClass.java
@@ -0,0 +1,22 @@
+//들여쓰기 전체 자동정렬 ctrl + shift + f
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 한 줄 주석
+		// System.out.println("11111");
+		// System.out.println("22222");
+
+		// 여러 줄 주석 
+		/* 드래그 후 ctrl + shift + /
+		 * System.out.println("33333"); System.out.println("44444");
+		 */
+		
+		// 원한는 줄 드래그 후 ctrl + /
+//		  System.out.println("555555555");
+//        System.out.println("6666666666666");
+	}
+
+}
