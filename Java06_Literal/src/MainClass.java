0aa26b0b326fa0b63d4403dac044babd250c275c
june0103 <rhwns0103@naver.com>
2023-04-26 11:01:26 +0900

    Literal

--- Java06_Literal/src/MainClass.java
+++ Java06_Literal/src/MainClass.java
@@ -16,11 +16,36 @@ public class MainClass {
 		System.out.printf("정수 : %d\n",100);
 		System.out.printf("정수 : %d, %d\n",200,300);
 		
+		// 정수를 작성하면 메모리에 4byte 기억공간이 마련되어 저장된다.
+		// 총 42억개의 숫자 표현이 가능하다.
 		System.out.printf("정수최대 : %d\n", 2147483647);
 		System.out.printf("정수최소 : %d\n", -2147483648);
 		
-		// 실수 리터럴
+		// 숫자 뒤에 l 이나 L 을 붙여주면 8byte 기억공간이 마련되어 저장된다.
+		// 총 약 1900경개의 숫자 표현이 가능하다.
+		System.out.printf("long 최대 : $d\n", 9223372036854775807L);
+		System.out.printf("long 최소 : $d\n", -9223372036854775808L);
 		
+		// 문자 2byte
+		// 작은 따옴표로 작성한다.
+		System.out.printf("문자 : %c\n", 'a');
+		System.out.printf("문자 : %c\n", '가');
+		
+		// 실수 8byte
+		System.out.printf("실수 : %f\n", 11.11);
+		
+		// 실수 4byte
+		// 실수 뒤에 f나 F를 붙이면 4byte 기억공간을 사용한다.
+		System.out.printf("float : %f\n" , 11.11F);
+		
+		// 논리값 (1byte)
+        System.out.printf("논리형 : %s\n", true);
+        System.out.printf("논리형 : %s\n", false);
+        
+        // 문자열
+        // 문자열은 값이 아니다. " " 로 묶어준다.
+        System.out.printf("문자열: %s\n", "반갑습니다");
+        
 		
 	}
 
